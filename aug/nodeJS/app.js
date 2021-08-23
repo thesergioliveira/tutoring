35883f61f0d77a586ae1b9908a44649d384692bc
thesergioliveira <thesergioliveira@gmail.com>
2021-08-24 00:43:56 +0200

    deleting some files

--- aug/nodeJS/app.js
+++
@@ -1,8 +0,0 @@
-// Importing from module.exports
-
-const sayHi = require("./util.js");
-// const names = require("./names.js");
-const { john, peter } = require("./names.js");
-
-sayHi(john);
-sayHi(peter);
--- aug/nodeJS/names.js
+++
@@ -1,8 +0,0 @@
-// local
-const sergio = "sergio";
-const john = "john";
-const peter = "peter";
-
-// share
-// console.log(module);
-module.exports = { john, peter };
