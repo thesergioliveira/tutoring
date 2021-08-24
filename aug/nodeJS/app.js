2b7e0b962444228e14737f6659ba2e2ee3632b17
thesergioliveira <thesergioliveira@gmail.com>
2021-08-24 18:02:24 +0200

    code prep for the class

--- aug/nodeJS/app.js
+++ aug/nodeJS/app.js
@@ -8,3 +8,12 @@ HTTP
 
 Further documentation on the modules that Node.js offers: https://nodejs.org/dist/latest-v14.x/docs/api/
 */
+
+const { readFile, writeFile } = require("fs");
+readFile("./content/first.txt", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+});
--- aug/nodeJS/modules.js
+++ aug/nodeJS/modules.js
@@ -1,7 +1,7 @@
-const sayHi = require("../util");
+const sayHi = require("./util");
 // const names = require("./names.js");
-const { john, peter } = require("../names.js");
+const { john, peter } = require("./names.js");
 
 sayHi(john);
 sayHi(peter);
-require("../mind-grenade");
+require("./mind-grenade");
