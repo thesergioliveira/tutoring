75be1b518bf21bf4f3da2b5326b01bd9e27d3fb2
thesergioliveira <thesergioliveira@gmail.com>
2021-09-08 23:33:52 +0200

    debugging the presentation with the reduce function

--- sep/oxford-dictionary/app.js
+++ sep/oxford-dictionary/app.js
@@ -2,3 +2,9 @@
 const apiFetch = require("./dictionary");
 const word = process.argv.slice(2).join("").toLowerCase();
 apiFetch(word);
+// apiFetch(word).then(console.log).catch(console.error)
+
+
+
+
+
